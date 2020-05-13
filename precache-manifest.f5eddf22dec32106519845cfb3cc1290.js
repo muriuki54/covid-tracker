833b3bb5a747750c6a275a909f01c4e64a221d89
Charles Muriuki <[muriukicharles54@gmail.com]>
2020-05-13 11:34:16 +0300

    Updates

---
+++ precache-manifest.f5eddf22dec32106519845cfb3cc1290.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae463fd5681dafaa05848a0e844736c4",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "bc0d94242e7febb2c4c7",
+    "url": "/covid-tracker/static/css/main.adea327e.chunk.css"
+  },
+  {
+    "revision": "2ebaecce1d6fd5b48e92",
+    "url": "/covid-tracker/static/js/2.d6231c98.chunk.js"
+  },
+  {
+    "revision": "2c24acf63e18487b21e343b8335b2718",
+    "url": "/covid-tracker/static/js/2.d6231c98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc0d94242e7febb2c4c7",
+    "url": "/covid-tracker/static/js/main.e049c010.chunk.js"
+  },
+  {
+    "revision": "1ee3619b46a3284fdd0e",
+    "url": "/covid-tracker/static/js/runtime-main.d20c9eb4.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-tracker/static/media/covid.d7265326.png"
+  }
+]);
